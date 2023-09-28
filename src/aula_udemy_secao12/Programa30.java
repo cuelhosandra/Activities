b44f40794fd73994ef2741d21a621451c923d922
Sandra Coelho <sandracoelho_br@hotmail.com>
2023-09-28 19:34:08 -0300

    secao 12 - aula sobre sobrescrita de método

---
+++ src/aula_udemy_secao12/Programa30.java
@@ -0,0 +1,19 @@
+package aula_udemy_secao12;
+//Sobrescrita de Método
+
+/*
+ * Overriding
+ * 
+ * Ocorre quando sobrescrevermos métodos da classe pai em classes filhas.
+ * 
+ * Exemplo: @Override
+ */
+public class Programa30 {
+
+	public static void main(String[] args) {
+	
+		
+
+	}
+
+}
